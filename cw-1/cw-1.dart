bf2df5b1fd577a9151773c0e7d3ba0598ea6fc8a
Ehmaid-Jarrah <113725964+Ehmaid-Jarrah@users.noreply.github.com>
2023-07-13 17:16:36 +0300

    claswork1

---
+++ cw-1/cw-1.dart
@@ -0,0 +1,35 @@
+
+void main() {
+String firstName = "EHMAID";
+String lastName = "jarrah";
+int age= 17;
+String hobby = 'Programming';
+print('First Name: $firstName'); 
+print('Last Name: $lastName');
+print('I\'m $age years old');
+print('My $hobby is $hobby');
+  }
+
+
+  void main(){
+void main() {
+ double tempF = -30.0; 
+  double tempC = (tempF - 32) / 1.8;
+  print('$tempF°F is equal to $tempC°C');
+  }
+
+void main() {
+  double weight = 70;
+  double height = 1.75;
+  double bmi = weight / (height * height);
+  if (bmi < 18.5) {
+    print('BMI: $bmi');
+    print('Underweight');
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    print('BMI: $bmi');
+    print('Normal weight');
+  } else {
+    print('BMI: $bmi');
+    print('Overweight');
+  }
+}
