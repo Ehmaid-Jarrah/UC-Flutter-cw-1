8d7e09814c3ba3c419859ce0aee6d4a0a40f394e
Ehmaid-Jarrah <113725964+Ehmaid-Jarrah@users.noreply.github.com>
2023-07-15 22:39:59 +0300

    c2flutter.dart

--- cw-2/cw-2.dart
+++ cw-2/cw-2.dart
@@ -1,1 +1,43 @@
+//هذا الكلاس ورك الاول
+void main() {
+  List<int> numbers = [];
 
+  for (int i = 1; i <= 5; i++) {
+    numbers.add(i);
+  }
+
+  print(numbers); // Output: [1, 2, 3, 4, 5]
+
+
+
+  List numberes1 = [1, 4, 2, 7];
+  int sum =0;
+ 
+ for (int x in numberes1){
+   sum = sum + x;
+ }
+
+ print(sum);
+}
+//و هذا الثاني
+void main() {
+  String getGrade(int i) {
+    var grade;
+    if (i >= 90) {
+      grade = 'A';
+    } else if (i >= 80) {
+      grade = 'B';
+    } else if (i >= 70) {
+      grade = 'C';
+    } else if (i >= 60) {
+      grade = 'D';
+    } else {
+      grade = 'F';
+    }
+
+    return grade;
+  }
+
+  var grade = getGrade(91);
+  print(grade); // Output: A
+}
